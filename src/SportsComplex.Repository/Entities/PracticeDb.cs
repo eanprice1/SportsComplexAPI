618c2cf0ee95cadc49a489ceebfcd1b831142e8e
eanprice <eanprice@iu.edu>
2022-11-29 11:40:41 -0500

    updated practice entity

--- src/SportsComplex.Repository/Entities/PracticeDb.cs
+++ src/SportsComplex.Repository/Entities/PracticeDb.cs
@@ -10,11 +10,10 @@ namespace SportsComplex.Repository.Entities
         public int Id { get; set; }
         public int TeamId { get; set; }
         public int? LocationId { get; set; }
-        public string DayOfWeek { get; set; }
-        [Column(TypeName = "Time")]
-        public TimeSpan StartTime { get; set; }
-        [Column(TypeName = "Time")]
-        public TimeSpan EndTime { get; set; }
+        [Column(TypeName = "DateTime")]
+        public DateTime StartDateTime { get; set; }
+        [Column(TypeName = "DateTime")]
+        public DateTime EndDateTime { get; set; }
         public TeamDb Team { get; set; }
         public LocationDb Location { get; set; }
     }
